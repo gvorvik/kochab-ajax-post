d524d9d4c41a02c9625a99f3955bd857bac4ae4e
Greg Orvik <gvorvik@gmail.com>
2018-04-19 11:51:29 -0500

    add records to dom via ajax

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,23 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQuery is here');
+    getRecords();
+}
+
+function getRecords() {
+    $.ajax({
+        method: "GET",
+        url: "/records"
+    })
+    .then((response) => {
+        for(let record of response) {
+            $('#recordList').append(`<li>${record.title} by ${record.artist} 
+                recorded in ${record.year} and costs $${record.cost}.</li>`);
+        }
+        for(let record of response) {
+            $('#recordTable').append(`<tr><td>${record.title}</td>
+                <td>${record.artist}</td><td>${record.year}</td>
+                <td>${record.cost}</td></tr>`);
+        }
+    });
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+const recordCollection = require('./modules/record-collection');
+
 app.use(express.static('server/public'));
+
+//routes
+app.get('/records', (req, res) => {
+    res.send(recordCollection);
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
